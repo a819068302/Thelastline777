fae97e7835a12f8b9e8e08f3e1ee5181d61061be
lilinmin1 <1048330609@qq.com>
2019-08-28 11:25:36 +0800

    增加组件

--- hello/src/App.vue
+++ hello/src/App.vue
@@ -1,29 +1,65 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="tabbar" >
+      <template v-for="(item,index) in tabbar" >
+        <router-link :to="item.path" :key="index">
+          <span class="T1">{{item.title}}</span>
+        </router-link>
+      </template>
     </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import {mapState} from 'vuex'
+export default {
+  data() {
+    return {
+      tabbar: [
+        {title: "邹", path: "/zou",name:"zou"},
+        {title: "李", path: "/li",name:"li"},
+        {title: "王", path: "/wang",name:"wang"},
+        {title: "冯", path: "/feng",name:"feng"},
+      ]
+    }
+  },
+  computed: {
+    ...mapState(['show'])
+  },
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+</script>
+
+<style lang="scss">
+  #tabbar{
+    z-index:99;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    position: fixed;
+    left: 0;
+    right: 0;
+    height: 48px;
+    border-top: 1px solid #d8d8d8;
+    background: #fff;
+    a{
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+      flex: 1;
+      height: 100%;
+      color: #696969;
+      span{
+        font-size: 20px;
+      }
+      &.router-link-active{
+        color: #f03d37;
+      }
     }
   }
-}
+  a{
+    color: #000;
+    text-decoration: none;
+  }
 </style>
--- hello/src/router.js
+++ hello/src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -9,17 +8,14 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path:'li',
+      name:'li',
+      component: () => import(/* webpackChunkName: "comingsoon" */ '@/components/li/li.vue')  //路由懒加载
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path:'comimgsoon',
+          name:'comingsoon',
+          component: () => import(/* webpackChunkName: "comingsoon" */ '@/components/zou/zou.vue')  //路由懒加载
     }
   ]
 })
